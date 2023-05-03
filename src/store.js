d1d6a83df6008c8ff52a38d94470fa5d765b43c1
Abhinavxox <abhinavxox@gmail.com>
2023-05-03 16:49:51 +0530

    <Added>:[Redux connected with firebase , created global state]

---
+++ src/store.js
@@ -0,0 +1,23 @@
+import {
+  legacy_createStore as createStore,
+  combineReducers,
+  applyMiddleware,
+} from "redux";
+import thunk from "redux-thunk";
+import { composeWithDevTools } from "redux-devtools-extension";
+
+import { authReducer } from "./reducers/userReducers";
+const reducer = combineReducers({
+  auth: authReducer,
+});
+
+let initialState = {};
+
+const middleware = [thunk];
+const store = createStore(
+  reducer,
+  initialState,
+  composeWithDevTools(applyMiddleware(...middleware))
+);
+
+export default store;
